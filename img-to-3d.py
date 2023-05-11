462508330317b0f30ec0f9f88024e90b5abd27ec
inf111 <inf111@mail.ru>
2023-05-11 18:07:50 +0300

    Added img-to-3d script

--- img-to-3d.py
+++ img-to-3d.py
@@ -1,30 +1,29 @@
 #!/usr/bin/env python3
-#https://github.com/openai/shap-e/blob/main/shap_e/examples/sample_text_to_3d.ipynb
-print("===START===")
-
+from shap_e.util.image_util import load_image
 import torch
-
 from shap_e.diffusion.sample import sample_latents
 from shap_e.diffusion.gaussian_diffusion import diffusion_from_config
 from shap_e.models.download import load_model, load_config
 from shap_e.util.notebooks import create_pan_cameras, decode_latent_images, gif_widget
 
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+print("===START===")
 
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 xm = load_model('transmitter', device=device)
 model = load_model('text300M', device=device)
 diffusion = diffusion_from_config(load_config('diffusion'))
 
 batch_size = 4
-guidance_scale = 15.0
-prompt = "a shark"
+guidance_scale = 3.0
+
+image = load_image("./shap_e/examples/example_data/corgi.png")
 
 latents = sample_latents(
     batch_size=batch_size,
     model=model,
     diffusion=diffusion,
     guidance_scale=guidance_scale,
-    model_kwargs=dict(texts=[prompt] * batch_size),
+    model_kwargs=dict(images=[image] * batch_size),
     progress=True,
     clip_denoised=True,
     use_fp16=True,
@@ -35,14 +34,13 @@ latents = sample_latents(
     s_churn=0,
 )
 
-render_mode = 'nerf' # you can change this to 'stf'
+render_mode = 'nerf' # you can change this to 'stf' for mesh rendering
 size = 64 # this is the size of the renders; higher values take longer to render.
 
 cameras = create_pan_cameras(size, device)
-for i, latent in enumerate(latents):
-    decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
-    images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
 
+for i, latent in enumerate(latents):
+    images = decode_latent_images(xm, latent, cameras, rendering_mode=render_mode)
     #display(gif_widget(images))
     gif_widget(images)
 
